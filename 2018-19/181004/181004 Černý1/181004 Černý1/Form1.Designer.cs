0d71abc97b8f8db3ef8fc75d7d1de9c143008aaa
Jan Černý <thewest123lp@email.cz>
2018-10-05 20:13:50 +0200

    Added 181004

---
+++ 2018-19/181004/181004 Černý1/181004 Černý1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace _181004_Černý1
+{
+    partial class FormBarvy
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnMagenta = new System.Windows.Forms.Button();
+            this.btnCyan = new System.Windows.Forms.Button();
+            this.btnRndYellow = new System.Windows.Forms.Button();
+            this.btnRndMagenta = new System.Windows.Forms.Button();
+            this.btnRndCyan = new System.Windows.Forms.Button();
+            this.btnRndBlue = new System.Windows.Forms.Button();
+            this.btnRndGreen = new System.Windows.Forms.Button();
+            this.btnRndRed = new System.Windows.Forms.Button();
+            this.btnRndRGB = new System.Windows.Forms.Button();
+            this.btnRndARGB = new System.Windows.Forms.Button();
+            this.btnRndGrey = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(12, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(70, 30);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(12, 48);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(70, 30);
+            this.btnGreen.TabIndex = 1;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(12, 84);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(70, 30);
+            this.btnBlue.TabIndex = 2;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(88, 84);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(70, 30);
+            this.btnYellow.TabIndex = 5;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnMagenta
+            // 
+            this.btnMagenta.Location = new System.Drawing.Point(88, 48);
+            this.btnMagenta.Name = "btnMagenta";
+            this.btnMagenta.Size = new System.Drawing.Size(70, 30);
+            this.btnMagenta.TabIndex = 4;
+            this.btnMagenta.Text = "Magenta";
+            this.btnMagenta.UseVisualStyleBackColor = true;
+            this.btnMagenta.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnCyan
+            // 
+            this.btnCyan.Location = new System.Drawing.Point(88, 12);
+            this.btnCyan.Name = "btnCyan";
+            this.btnCyan.Size = new System.Drawing.Size(70, 30);
+            this.btnCyan.TabIndex = 3;
+            this.btnCyan.Text = "Cyan";
+            this.btnCyan.UseVisualStyleBackColor = true;
+            this.btnCyan.Click += new System.EventHandler(this.btnPojmenovane_Click);
+            // 
+            // btnRndYellow
+            // 
+            this.btnRndYellow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndYellow.Location = new System.Drawing.Point(356, 84);
+            this.btnRndYellow.Name = "btnRndYellow";
+            this.btnRndYellow.Size = new System.Drawing.Size(70, 30);
+            this.btnRndYellow.TabIndex = 11;
+            this.btnRndYellow.Text = "11B";
+            this.btnRndYellow.UseVisualStyleBackColor = true;
+            this.btnRndYellow.Click += new System.EventHandler(this.btnRndYellow_Click);
+            // 
+            // btnRndMagenta
+            // 
+            this.btnRndMagenta.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndMagenta.Location = new System.Drawing.Point(356, 48);
+            this.btnRndMagenta.Name = "btnRndMagenta";
+            this.btnRndMagenta.Size = new System.Drawing.Size(70, 30);
+            this.btnRndMagenta.TabIndex = 10;
+            this.btnRndMagenta.Text = "1G1";
+            this.btnRndMagenta.UseVisualStyleBackColor = true;
+            this.btnRndMagenta.Click += new System.EventHandler(this.btnRndMagenta_Click);
+            // 
+            // btnRndCyan
+            // 
+            this.btnRndCyan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndCyan.Location = new System.Drawing.Point(356, 12);
+            this.btnRndCyan.Name = "btnRndCyan";
+            this.btnRndCyan.Size = new System.Drawing.Size(70, 30);
+            this.btnRndCyan.TabIndex = 9;
+            this.btnRndCyan.Text = "R11";
+            this.btnRndCyan.UseVisualStyleBackColor = true;
+            this.btnRndCyan.Click += new System.EventHandler(this.btnRndCyan_Click);
+            // 
+            // btnRndBlue
+            // 
+            this.btnRndBlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndBlue.Location = new System.Drawing.Point(280, 84);
+            this.btnRndBlue.Name = "btnRndBlue";
+            this.btnRndBlue.Size = new System.Drawing.Size(70, 30);
+            this.btnRndBlue.TabIndex = 8;
+            this.btnRndBlue.Text = "00B";
+            this.btnRndBlue.UseVisualStyleBackColor = true;
+            this.btnRndBlue.Click += new System.EventHandler(this.btnRndBlue_Click);
+            // 
+            // btnRndGreen
+            // 
+            this.btnRndGreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndGreen.Location = new System.Drawing.Point(280, 48);
+            this.btnRndGreen.Name = "btnRndGreen";
+            this.btnRndGreen.Size = new System.Drawing.Size(70, 30);
+            this.btnRndGreen.TabIndex = 7;
+            this.btnRndGreen.Text = "0G0";
+            this.btnRndGreen.UseVisualStyleBackColor = true;
+            this.btnRndGreen.Click += new System.EventHandler(this.btnRndGreen_Click);
+            // 
+            // btnRndRed
+            // 
+            this.btnRndRed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndRed.Location = new System.Drawing.Point(280, 12);
+            this.btnRndRed.Name = "btnRndRed";
+            this.btnRndRed.Size = new System.Drawing.Size(70, 30);
+            this.btnRndRed.TabIndex = 6;
+            this.btnRndRed.Text = "R00";
+            this.btnRndRed.UseVisualStyleBackColor = true;
+            this.btnRndRed.Click += new System.EventHandler(this.btnRndRed_Click);
+            // 
+            // btnRndRGB
+            // 
+            this.btnRndRGB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndRGB.Location = new System.Drawing.Point(164, 12);
+            this.btnRndRGB.Name = "btnRndRGB";
+            this.btnRndRGB.Size = new System.Drawing.Size(110, 30);
+            this.btnRndRGB.TabIndex = 12;
+            this.btnRndRGB.Text = "Náhodně RGB";
+            this.btnRndRGB.UseVisualStyleBackColor = true;
+            this.btnRndRGB.Click += new System.EventHandler(this.btnRndRGB_Click);
+            // 
+            // btnRndARGB
+            // 
+            this.btnRndARGB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndARGB.Location = new System.Drawing.Point(164, 48);
+            this.btnRndARGB.Name = "btnRndARGB";
+            this.btnRndARGB.Size = new System.Drawing.Size(110, 30);
+            this.btnRndARGB.TabIndex = 13;
+            this.btnRndARGB.Text = "Náhodně ARGB";
+            this.btnRndARGB.UseVisualStyleBackColor = true;
+            this.btnRndARGB.Click += new System.EventHandler(this.btnRndARGB_Click);
+            // 
+            // btnRndGrey
+            // 
+            this.btnRndGrey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRndGrey.Location = new System.Drawing.Point(164, 84);
+            this.btnRndGrey.Name = "btnRndGrey";
+            this.btnRndGrey.Size = new System.Drawing.Size(110, 30);
+            this.btnRndGrey.TabIndex = 14;
+            this.btnRndGrey.Text = "Náhodně Šedá";
+            this.btnRndGrey.UseVisualStyleBackColor = true;
+            this.btnRndGrey.Click += new System.EventHandler(this.btnRndGrey_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOut.Location = new System.Drawing.Point(12, 121);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(414, 143);
+            this.lblOut.TabIndex = 15;
+            this.lblOut.Text = "label1";
+            this.lblOut.BackColorChanged += new System.EventHandler(this.lblOut_BackColorChanged);
+            // 
+            // FormBarvy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 273);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnRndGrey);
+            this.Controls.Add(this.btnRndARGB);
+            this.Controls.Add(this.btnRndRGB);
+            this.Controls.Add(this.btnRndYellow);
+            this.Controls.Add(this.btnRndMagenta);
+            this.Controls.Add(this.btnRndCyan);
+            this.Controls.Add(this.btnRndBlue);
+            this.Controls.Add(this.btnRndGreen);
+            this.Controls.Add(this.btnRndRed);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnMagenta);
+            this.Controls.Add(this.btnCyan);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnRed);
+            this.Name = "FormBarvy";
+            this.Text = "Barvy";
+            this.Load += new System.EventHandler(this.FormBarvy_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnMagenta;
+        private System.Windows.Forms.Button btnCyan;
+        private System.Windows.Forms.Button btnRndYellow;
+        private System.Windows.Forms.Button btnRndMagenta;
+        private System.Windows.Forms.Button btnRndCyan;
+        private System.Windows.Forms.Button btnRndBlue;
+        private System.Windows.Forms.Button btnRndGreen;
+        private System.Windows.Forms.Button btnRndRed;
+        private System.Windows.Forms.Button btnRndRGB;
+        private System.Windows.Forms.Button btnRndARGB;
+        private System.Windows.Forms.Button btnRndGrey;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ 2018-19/181004/181004 Černý1/181004 Černý1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _181004_Černý1
+{
+    public partial class FormBarvy : Form
+    {
+        static Random _ran = new Random();
+        public FormBarvy()
+        {
+            InitializeComponent();
+        }
+
+        private void FormBarvy_Load(object sender, EventArgs e)
+        {
+            MinimumSize = Size;
+
+            lblOut.Text = "Label pro výpis\n";
+            lblOut.Text += "Změněné vlastnosti:\n";
+            lblOut.Text += " - AutoSize\n";
+            lblOut.Text += " - BorderStyle\n";
+            lblOut.Text += "\n";
+            lblOut.Text += "Rada:\n";
+            lblOut.Text += "Pro výpis do labelu:\n";
+            lblOut.Text += ">> labelu přiřaďte metodu k události BackColorChanged a vypisujte v ní\n";
+            lblOut.Text += ">> hodnoty jednotlivých komponent barvy získáte z vlastností barvy\n";
+
+            btnRed.BackColor = Color.Red;
+            btnGreen.BackColor = Color.Green;
+            btnBlue.BackColor = Color.Blue;
+
+            btnCyan.BackColor = Color.Cyan;
+            btnMagenta.BackColor = Color.Magenta;
+            btnYellow.BackColor = Color.Yellow;
+
+            btnRndRGB.BackColor = Color.FromArgb(_ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256));
+            btnRndARGB.BackColor = Color.FromArgb(_ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256));
+            int grey = _ran.Next(0, 256);
+            btnRndGrey.BackColor = Color.FromArgb(grey, grey, grey);
+
+            btnRndRed.BackColor = Color.FromArgb(_ran.Next(0, 256), 0, 0);
+            btnRndGreen.BackColor = Color.FromArgb(0, _ran.Next(0, 256), 0);
+            btnRndBlue.BackColor = Color.FromArgb(0, 0, _ran.Next(0, 256));
+
+            btnRndCyan.BackColor = Color.FromArgb(_ran.Next(0, 256), 255, 255);
+            btnRndMagenta.BackColor = Color.FromArgb(255, _ran.Next(0, 256), 255);
+            btnRndYellow.BackColor = Color.FromArgb(255, 255, _ran.Next(0, 256));
+        }
+
+        #region PojmenovaneBarvy
+        private void btnPojmenovane_Click(object sender, EventArgs e)
+        {
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        #endregion
+        
+        #region NahodneBarvy
+        private void btnRndRGB_Click(object sender, EventArgs e)
+        {
+            btnRndRGB.BackColor = Color.FromArgb(_ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256));
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndARGB_Click(object sender, EventArgs e)
+        {
+            btnRndARGB.BackColor = Color.FromArgb(_ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256), _ran.Next(0, 256));
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndGrey_Click(object sender, EventArgs e)
+        {
+            int seda = _ran.Next(0, 256);
+            btnRndGrey.BackColor = Color.FromArgb(seda, seda, seda);
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+        #endregion
+
+        #region NahodnyJedenKanal
+        private void btnRndRed_Click(object sender, EventArgs e)
+        {
+            btnRndRed.BackColor = Color.FromArgb(_ran.Next(0, 256), 0, 0);
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndGreen_Click(object sender, EventArgs e)
+        {
+            btnRndGreen.BackColor = Color.FromArgb(0, _ran.Next(0, 256), 0);
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndBlue_Click(object sender, EventArgs e)
+        {
+            btnRndBlue.BackColor = Color.FromArgb(0, 0, _ran.Next(0, 256));
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndCyan_Click(object sender, EventArgs e)
+        {
+            btnRndCyan.BackColor = Color.FromArgb(_ran.Next(0, 256), 255, 255);
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndMagenta_Click(object sender, EventArgs e)
+        {
+            btnRndMagenta.BackColor = Color.FromArgb(255, _ran.Next(0, 256), 255);
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+
+        private void btnRndYellow_Click(object sender, EventArgs e)
+        {
+            btnRndYellow.BackColor = Color.FromArgb(255, 255, _ran.Next(0, 256));
+            lblOut.BackColor = ((Button)sender).BackColor;
+        }
+        #endregion
+
+        private void lblOut_BackColorChanged(object sender, EventArgs e)
+        {
+            //Když je barva pozadí moc tmavá, nastaví text na bílou a naopak
+            lblOut.ForeColor = lblOut.BackColor.GetBrightness() < 0.5 && lblOut.BackColor.A > 128 ? Color.White : DefaultForeColor;
+
+            lblOut.Text = "Nastavená barva:\n";
+            lblOut.Text += $"{lblOut.BackColor}\n";
+            lblOut.Text += $"A: {lblOut.BackColor.A}\n";
+            lblOut.Text += $"R: {lblOut.BackColor.R}\n";
+            lblOut.Text += $"G: {lblOut.BackColor.G}\n";
+            lblOut.Text += $"B: {lblOut.BackColor.B}\n";
+        }
+    }
+}
