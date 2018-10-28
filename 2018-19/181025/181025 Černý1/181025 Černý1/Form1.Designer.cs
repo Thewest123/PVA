ba05c65d7adeb3a2c9c871993b27e9d4563bda32
Jan Černý <thewest123lp@email.cz>
2018-10-28 15:36:15 +0100

    Added 181025

---
+++ 2018-19/181025/181025 Černý1/181025 Černý1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _181025_Černý1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.tbWidth = new System.Windows.Forms.TrackBar();
+            this.tbHeight = new System.Windows.Forms.TrackBar();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnSelectPicture, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSelectColor, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 412);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(653, 38);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectPicture.Location = new System.Drawing.Point(3, 3);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(320, 32);
+            this.btnSelectPicture.TabIndex = 0;
+            this.btnSelectPicture.Text = "Vybrat obrázek";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnSelectPicture_Click);
+            // 
+            // btnSelectColor
+            // 
+            this.btnSelectColor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectColor.Location = new System.Drawing.Point(329, 3);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(321, 32);
+            this.btnSelectColor.TabIndex = 1;
+            this.btnSelectColor.Text = "Vybrat barvu";
+            this.btnSelectColor.UseVisualStyleBackColor = true;
+            this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+            // 
+            // tbWidth
+            // 
+            this.tbWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbWidth.Location = new System.Drawing.Point(63, 13);
+            this.tbWidth.Name = "tbWidth";
+            this.tbWidth.Size = new System.Drawing.Size(602, 45);
+            this.tbWidth.TabIndex = 1;
+            this.tbWidth.Scroll += new System.EventHandler(this.tbWidth_Scroll);
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbHeight.Location = new System.Drawing.Point(12, 61);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tbHeight.Size = new System.Drawing.Size(45, 345);
+            this.tbHeight.TabIndex = 2;
+            this.tbHeight.Scroll += new System.EventHandler(this.tbHeight_Scroll);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(63, 64);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(602, 338);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 462);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.tbWidth);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "PictureBox";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tbWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.TrackBar tbWidth;
+        private System.Windows.Forms.TrackBar tbHeight;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ 2018-19/181025/181025 Černý1/181025 Černý1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _181025_Černý1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MinimumSize = Size;
+
+            tbWidth.Minimum = 0;
+            tbWidth.Maximum = pictureBox.Width;
+            tbWidth.Value = tbWidth.Maximum;
+
+            tbHeight.Minimum = -pictureBox.Height;
+            tbHeight.Maximum = 0;
+            tbHeight.Value = tbHeight.Minimum;
+        }
+
+        private void btnSelectPicture_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Obrázkové typy|*.jpg;*.jpeg;*.png;*.bmp;*.gif;*.tiff|Všechny soubory|*.*",
+                Title = "Vyberte obrázek"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            var colorDialog = new ColorDialog();
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = null;
+                pictureBox.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void tbWidth_Scroll(object sender, EventArgs e)
+        {
+            pictureBox.Width = tbWidth.Value;
+        }
+
+        private void tbHeight_Scroll(object sender, EventArgs e)
+        {
+            pictureBox.Height = Math.Abs(tbHeight.Value);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            tbWidth.Maximum = tbWidth.Width;
+            tbWidth.Value = pictureBox.Width;
+
+            tbHeight.Minimum = -tbHeight.Height;
+            tbHeight.Value = -pictureBox.Height;
+        }
+    }
+}
