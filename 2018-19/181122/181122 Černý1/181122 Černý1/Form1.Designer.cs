2aeb8fb2f52ecd0ec6ae8d86ca1849bd4441c245
Jan Černý <thewest123lp@email.cz>
2018-11-29 16:39:44 +0100

    Added 181122

---
+++ 2018-19/181122/181122 Černý1/181122 Černý1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _181122_Černý1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnVypis = new System.Windows.Forms.Button();
+            this.btnGeneruj = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnVypis, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnGeneruj, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(176, 29);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnVypis
+            // 
+            this.btnVypis.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnVypis.Location = new System.Drawing.Point(3, 3);
+            this.btnVypis.Name = "btnVypis";
+            this.btnVypis.Size = new System.Drawing.Size(82, 23);
+            this.btnVypis.TabIndex = 0;
+            this.btnVypis.Text = "Vypiš";
+            this.btnVypis.UseVisualStyleBackColor = true;
+            this.btnVypis.Click += new System.EventHandler(this.btnVypis_Click);
+            // 
+            // btnGeneruj
+            // 
+            this.btnGeneruj.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGeneruj.Location = new System.Drawing.Point(91, 3);
+            this.btnGeneruj.Name = "btnGeneruj";
+            this.btnGeneruj.Size = new System.Drawing.Size(82, 23);
+            this.btnGeneruj.TabIndex = 1;
+            this.btnGeneruj.Text = "Generuj";
+            this.btnGeneruj.UseVisualStyleBackColor = true;
+            this.btnGeneruj.Click += new System.EventHandler(this.btnGeneruj_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutput.Location = new System.Drawing.Point(12, 44);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(176, 217);
+            this.rtbOutput.TabIndex = 1;
+            this.rtbOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 273);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnVypis;
+        private System.Windows.Forms.Button btnGeneruj;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ 2018-19/181122/181122 Černý1/181122 Černý1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _181122_Černý1
+{
+    public partial class Form1 : Form
+    {
+        int dolniMez = -100;
+        int horniMez = 100;
+        int pocet = 10;
+        int[] pole = new int[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string zprava = @"Ano 
+>> načte soubor (čOd*čDo*čPočet)
+>> vytvoří pole, vygeneruje do něj čísla
+>> hlídá pasivně chyby >> jako Ne
+Ne
+>> vytvoří pole <10,20>
+>> vygeneruje do něj čísla <<-100,-1>,<1,100>>";
+
+            if (MessageBox.Show(zprava, "Chcete načíst hodnoty?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Text = Nacti();
+            }
+            else
+            {
+                Generuj();
+                Text = "Vygenerováno";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string zprava = @"Ano 
+>> přepíše soubor hodnotami (čOd*čDo*čPočet)
+Ne
+>> neuloží hodnoty do souboru
+Storno >> zastaví zavírání";
+
+            DialogResult dr = MessageBox.Show(zprava, "Chcete uložit změny?", MessageBoxButtons.YesNoCancel);
+
+            if (dr == DialogResult.Yes)
+            {
+                File.WriteAllText("t.txt", $"{dolniMez}*{horniMez}*{pocet}");
+
+                using (StreamWriter sw = new StreamWriter("tt.txt"))
+                {
+                    foreach (var c in pole)
+                    {
+                        sw.WriteLine(c);
+                    }
+                }
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnGeneruj_Click(object sender, EventArgs e)
+        {
+            Generuj();
+        }
+
+        private void btnVypis_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Text = $@"Pole:
+Velikost {pole.Length}
+Dolní mez: {dolniMez}
+Horní mez: {horniMez}
+------------
+";
+            for (int i = 0; i < pole.Length; i++)
+            {
+                rtbOutput.Text += $"pole[{i}] <> {pole[i]}\n";
+            }
+        }
+
+        private void Generuj(bool nove = true)
+        {
+            Random r = new Random();
+            if (nove)
+            {
+                pocet = r.Next(10, 20 + 1);
+                dolniMez = r.Next(-100, 0 + 1);
+                horniMez = r.Next(1, 100 + 1);
+            }
+
+            pole = new int[pocet];
+
+            for (int i = 0; i < pocet; i++)
+            {
+                pole[i] = r.Next(dolniMez, horniMez + 1);
+            }
+        }
+
+        private string Nacti()
+        {
+            try
+            {
+                string text = File.ReadAllText("t.txt", Encoding.Default);
+
+                string[] split = text.Split('*');
+                dolniMez = Convert.ToInt32(split[0]);
+                horniMez = Convert.ToInt32(split[1]);
+                pocet = Convert.ToInt32(split[2]);
+
+                bool ano = NactiHodnoty();
+                if (ano) return "Načteno";
+                return "Vygenerováno";
+            }
+            catch
+            {
+                Generuj();
+                return "Vygenerováno";
+            }
+        }
+
+        private bool NactiHodnoty()
+        {
+            try
+            {
+                string[] poleHodnot = File.ReadAllLines("tt.txt", Encoding.Default);
+
+                if (poleHodnot.Length != pocet)
+                {
+                    MessageBox.Show("Počet se nerovná velikosti pole");
+                    Generuj();
+                    return false;
+                }
+                else
+                {
+                    bool ano = KontrolaHodnot(poleHodnot);
+                    
+                    if (ano) return true;
+
+                    MessageBox.Show("Kontrola hodnot je false");
+                    Generuj();
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error při načítání");
+                Generuj();
+                return false;
+            }
+        }
+
+        private bool KontrolaHodnot(string[] poleHodnot)
+        {
+            int pocetChybnychKonverzi = 0;
+            int[] novePoleHodnot = new int[poleHodnot.Length];
+
+            for (int i = 0; i < poleHodnot.Length; i++)
+            {
+                if (!int.TryParse(poleHodnot[i], out novePoleHodnot[i])) pocetChybnychKonverzi++;
+            }
+
+            if (pocetChybnychKonverzi == 0)
+            {
+                pole = novePoleHodnot;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
